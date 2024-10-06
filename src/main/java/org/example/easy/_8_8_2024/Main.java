804dd2a1ff035ec13cbc28b7f919855b279b8a7d
Yurii <leskiv.yurii@gmail.com>
2024-10-06 22:01:33 +0300

    _1_9_2024 [Yurii Leskiv] solved Delete Node in a BST

--- src/main/java/org/example/easy/_8_8_2024/Main.java
+++ src/main/java/org/example/easy/_8_8_2024/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 public class Main {
-
+    
     public static void main(String[] args) {
         System.out.println(findDifference(new int[] {1,2,3, 3} , new int[] {1, 1, 2, 2}));
     }

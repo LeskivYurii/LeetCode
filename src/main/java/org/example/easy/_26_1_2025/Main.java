4c253abe3959596757dfea37f1da6f44defac8c7
Yura <leskiv.yurii@gmail.com>
2025-01-27 01:04:34 +0200

    [Yurii Leskiv] Solver double bracketsi ssue

---
+++ src/main/java/org/example/easy/_26_1_2025/Main.java
@@ -0,0 +1,34 @@
+package org.example.easy._26_1_2025;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
